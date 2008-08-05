8693a9337df9be9cc688ec919674df83b7c91fbc
Kenneth Miller <xkenneth@xkenneth-desktop-buildoutbuilder.(none)>
2008-08-05 02:23:41 -0500

    Adding setup.py

---
+++ buildoutbuilder.grokapp/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.0'
+
+setup(name='buildoutbuilder',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[], 
+      keywords="",
+      author="",
+      author_email="",
+      url="",
+      license="",
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages = ['buildoutbuilder'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grok',
+                        'z3c.testsetup',
+                        'lxml'
+                        # Add extra requirements here
+                        ],
+      entry_points="""
+      # Add entry points here
+      """,
+      )
